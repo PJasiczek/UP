5379cfb43d6da828095f1a9c7946b89dfc5e24f3
PJasiczek <jasiu1047@gmail.com>
2018-11-27 19:57:38 +0100

    Program shows joystick operations and functions in the windows form application

---
+++ UP_lab10/UP_lab10/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace UP_lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X Position:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y Position:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(139, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(352, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 31);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Clicked";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(268, 149);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 39);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(499, 149);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 39);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "11";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(268, 289);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 39);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "7";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(499, 289);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 39);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "10";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(16, 391);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 48);
+            this.label10.TabIndex = 12;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(59, 116);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(79, 17);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Z Position: ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(139, 116);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(16, 17);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "0";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(343, 337);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(26, 29);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "8";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(448, 337);
+            this.label14.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(26, 29);
+            this.label14.TabIndex = 16;
+            this.label14.Text = "9";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(343, 194);
+            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(23, 25);
+            this.label15.TabIndex = 17;
+            this.label15.Text = "4";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(449, 194);
+            this.label16.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(23, 25);
+            this.label16.TabIndex = 18;
+            this.label16.Text = "5";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(393, 219);
+            this.label17.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(24, 26);
+            this.label17.TabIndex = 19;
+            this.label17.Text = "3";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(392, 262);
+            this.label18.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(27, 29);
+            this.label18.TabIndex = 20;
+            this.label18.Text = "2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Ćwiczenie 10 - Urządzenia Peryferyjne";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+    }
+}
+
---
+++ UP_lab10/UP_lab10/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SlimDX.DirectInput;
+using System.Runtime.InteropServices;
+
+namespace UP_lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetSticks();
+            sticks = GetSticks();
+            timer1.Enabled = true;
+        }
+        DirectInput input = new DirectInput();
+        SlimDX.DirectInput.Joystick stick;
+        Joystick[] sticks;
+        bool mouseClicked = false;
+        int xVal = 0;
+        int yVal = 0;
+        int zVal = 0;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern void mouse_event(uint flag, uint _x, uint _y, uint bnt, uint exInfo);
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x02;  
+        private const uint MOUSEEVENTF_LEFTUP = 0x04;  
+        private const uint MOUSEEVENTF_RIGHTDOWN = 0x08; 
+        private const uint MOUSEEVENTF_RIGHTUP = 0x10; 
+
+        public Joystick[] GetSticks()
+        {
+            List<SlimDX.DirectInput.Joystick> sticks = new List<SlimDX.DirectInput.Joystick>();
+            foreach(DeviceInstance device in input.GetDevices(DeviceClass.GameController,DeviceEnumerationFlags.AttachedOnly))
+            {
+                try
+                {
+                    stick = new SlimDX.DirectInput.Joystick(input, device.InstanceGuid);
+                    label10.Text = device.ProductName;
+                    stick.Acquire();
+                    foreach(DeviceObjectInstance deviceObject in stick.GetObjects()){
+
+                        if ((deviceObject.ObjectType & ObjectDeviceType.Axis) != 0)
+                        {
+                            stick.GetObjectPropertiesById((int)deviceObject.ObjectType).SetRange(-100, 100);
+         
+                        }
+                    }
+                    sticks.Add(stick);
+                }
+                catch (DirectInputException)
+                {
+
+                }
+            }
+            return sticks.ToArray();
+        }
+
+        void stickHandle(Joystick stick, int id)
+        {
+            JoystickState state = new JoystickState();
+            state = stick.GetCurrentState();
+            xVal = state.X;
+            yVal = state.Y;
+            zVal = state.Z;
+            MouseMove(xVal, yVal);
+            bool[] buttons = state.GetButtons();
+            label3.Text = xVal.ToString();
+            label4.Text = yVal.ToString();
+            label12.Text = zVal.ToString();
+            if (id == 0)
+            {
+                if(buttons[0])
+                {
+                   if(mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label5.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[1])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label18.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label18.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[2])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label17.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label17.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[3])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label15.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label15.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[4])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label16.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label16.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[5])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label6.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[6])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label8.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[7])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label13.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label13.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[8])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label14.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label14.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[9])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label9.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+                if (buttons[10])
+                {
+                    if (mouseClicked == false)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouseClicked = true;
+                        label7.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+                    }
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseClicked = false;
+                    this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+                }
+     
+            }
+        }
+
+        public void MouseMove(int posx, int posy)
+        {
+            Cursor.Position = new Point(Cursor.Position.X + posx/3, Cursor.Position.Y + posy/3);
+            Cursor.Clip = new Rectangle(this.Location, this.Size);
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for(int i = 0; i < sticks.Length; i++)
+            {
+                stickHandle(sticks[i], i);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Joystick[] joystick = GetSticks();
+        }
+
+    }
+}
